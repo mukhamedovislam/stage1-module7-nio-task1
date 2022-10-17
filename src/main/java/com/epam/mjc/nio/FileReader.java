1d2931a4e529c7d4f8263ab5f9c942a1c61ec2f2
mukhamedovislam <bozuy24@gmail.com>
2022-10-17 12:11:44 +0400

    FileReader.java update

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+	    Profile profile = new Profile();
+	    Path path = Paths.get(file.getPath());
+	    try(BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)){
+		    String currentLine = null;
+		    while((currentLine = reader.readLine()) != null){
+			    if(currentLine.contains("Name")){
+				    profile.setName(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length()));
+			    }
+			    if(currentLine.contains("Age")){
+				    profile.setAge(Integer.parseInt(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length())));
+			    }
+			    if(currentLine.contains("Email")){
+				    profile.setEmail(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length()));
+			    }
+			    if(currentLine.contains("Phone")){
+				    profile.setPhone(Long.parseLong(currentLine.substring(currentLine.indexOf(" ") + 1, currentLine.length())));
+			    }
+		    }
+	    } catch(IOException e){
+		    e.printStackTrace();
+	    }
+	    return profile;
     }
 }
